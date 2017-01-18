c91479e4fffe8b4ab3bb380525dd6cd8cfdd6be2
Aaron Perley <aaron.perley@gmail.com>
2017-01-18 02:20:34 -0500

    Add testing fish detection code

--- FishSensor.py
+++ FishSensor.py
@@ -1,3 +1,5 @@
+import numpy as np
+import cv2
 from DataTypes import FishPosition
 
 class FishSensor(object):
@@ -6,3 +8,26 @@ class FishSensor(object):
 
     def poll(self):
         return FishPosition(x=0, y=0)
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+
+    def onClick(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            print x, y, frame[y, x]
+
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image", onClick)
+
+    while True:
+        ret, frame = cap.read()
+        lower = np.array([0, 65, 130], dtype='uint8')
+        upper = np.array([40, 120, 200], dtype='uint8')
+        mask = cv2.inRange(frame, lower, upper)
+        output = cv2.bitwise_and(frame, frame, mask=mask)
+        cv2.imshow("image", output)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
